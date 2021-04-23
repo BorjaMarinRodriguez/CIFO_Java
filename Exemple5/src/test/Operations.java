c4472352467d2b877e1008013db56d6e1018e992
mati <mati@DESKTOP-7C9GJPB>
2021-04-23 11:20:02 +0200

    finish

--- Exemple5/src/test/Operations.java
+++ Exemple5/src/test/Operations.java
@@ -2,13 +2,7 @@ package test;
 
 import java.util.Scanner;
 
-public class Ticket {
-	// scope number #1
-	
-	public static void main(String[] args) {
-		// scope number #2
-		clientBuyProcess();
-	}
+public class Operations {
 
 	public static void clientBuyProcess() {
 		// scope number #3
@@ -19,8 +13,10 @@ public class Ticket {
 		while (true) {
 			// scope #4
 			Scanner reader = new Scanner(System.in);
-			System.out.println("Tell me your user name:");
+			System.out.println("Tell me your user name (quit to exit):");
 			String userName = reader.nextLine();
+			
+			if (userName.equals("quit")) break;
 
 			System.out.println("Tell me your type of user:");
 			String userType = reader.nextLine();
@@ -57,7 +53,7 @@ public class Ticket {
 
 			} else {
 				// scope number #8
-				System.out.println("Please, you have to write \"vip\", \"regular\" or \"new\". Try another time ...");
+				System.out.println("Please, you have to write \"vip\", \"regular\" or \"new\". Try another time ...\n");
 			}
 
 			// I am closing the while so also the while scope
@@ -65,35 +61,35 @@ public class Ticket {
 
 	}
 
-	public static void printTicket(int userBuy, int discount, int totalDiscount, int amountToPay) {
+	private static void printTicket(int userBuy, int discount, int totalDiscount, int amountToPay) {
 		// scope number #9
 		System.out.println("\nTicket:");
 		System.out.println("-------------------------\n");
 		System.out.println("The amount of your buy is: " + userBuy);
-		System.out.println("Your discount is: " + discount + "% of the total amount buoght");
+		System.out.println("Your discount is: " + discount + "% of the total amount bought");
 		System.out.println("Your total discount is: " + totalDiscount);
 		System.out.println("Your should pay (with the discount): " + amountToPay);
 		System.out.println("Your should pay (with taxes): " + addTax(amountToPay) + "\n");
-		
-		
+
 	}
 
-	public static int addTax (int amountToPay) {
+	private static double addTax(int amountToPay) {
 		// scope number #10
 		double tax = 0.21;
-		
-		//i am doing an EXPLICIT casting to be able to use amountToPay as a DOUBLE
-		//double taxToPay = (double) amountToPay * tax;
-		//System.out.println(taxToPay);
-				
-		//i don't need an EXPLICIT casting cause taxToPay is double so it has ... decs...
-		double taxToPay =  amountToPay * tax;
-		
+
+		// i am doing an EXPLICIT casting to be able to use amountToPay as a DOUBLE
+		// double taxToPay = (double) amountToPay * tax;
+
+		// i don't need an EXPLICIT casting cause taxToPay is double so it has ...
+		// decimals...
+		double taxToPay = amountToPay * tax;
+		System.out.println("Your should pay taxes: " + taxToPay);
+
 		double amountToPayWithTaxes = amountToPay + taxToPay;
-		
-		return (int) amountToPayWithTaxes ;
-		
+
+		return amountToPayWithTaxes;
+
 	}
 	
-	
+
 }
