95a5e49cfa724bd93f45d8687deac4d080e23161
mati <mati@DESKTOP-7C9GJPB>
2021-04-29 12:40:39 +0200

    commit 1

--- WordsOperations1/src/wordsOperations/Init.java
+++ WordsOperations1/src/wordsOperations/Init.java
@@ -2,24 +2,20 @@ package wordsOperations;
 
 import java.util.Scanner;
 
+
 public class Init {
 	// scope #1
 	public static void main(String[] args) {
 
 		// scope #2
 		Scanner reader = new Scanner(System.in);
-		System.out.println("Write some string ...");
-		String word = reader.nextLine();
-
-		//Operations.operationsString(word);
-		//Editing.subEditing (word);
-		EmailManagment.createAccount(word);
 		
+		System.out.println("Type user (name and surname) to create Account ...");
+		String userToCreateAccount = reader.nextLine();
+
+		AccountManagment.createAccount(userToCreateAccount);
 		
 		System.out.println("\nBye bye my friend...");
-
+		reader.close();
 	}
-
-	
-
 }
